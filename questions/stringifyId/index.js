1460d34cca7772e56e117c01559d8d5f1217d761
Moe-Zart <mohamedzreika03@outlook.com>
2022-08-03 22:11:58 +1000

    stringify id

--- questions/stringifyId/index.js
+++ questions/stringifyId/index.js
@@ -17,7 +17,16 @@
  */
 
 const stringifyId = (arr) => {
-
+//convert the array of objects into an array of ids
+    let arrayId = arr.map(obj=> obj.id)
+//convert the array of ids into an array of unique ids
+    let arrayUniqueId = [] //new array to push unique ids into
+    for(let i of arrayId) { //loop on iterable objects (e.g:string, array, map, etc) (note: normal for loop is on the indexes)
+        if(!arrayUniqueId.includes(i)){
+            arrayUniqueId.push(i)
+        }
+    }
+return arrayUniqueId.join(", ")
 };
 
 module.exports = stringifyId;
