cd11e90e962872153d3d5d47ffd4ae281e10c4e5
Moe-Zart <mohamedzreika03@outlook.com>
2022-08-03 22:55:25 +1000

    maxCharacter

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -1,4 +1,4 @@
-/** Given a string, find the character that is used the 
+/** Given a string, find the character that is used the
  * maximum number of times.
  *
  * @example
@@ -7,7 +7,22 @@
  */
 
 const maxCharacter = (str) => {
+    //S1mpl1f1ed
+  let charMap = {}; // empty object
+  for (let i of str) { //loops through every iterable object of str (each letter)
+    !charMap[i] ? charMap[i] = 1 : charMap[i] += 1 //if there is no charMap[letter], then return the letter with value of one, otherwise, increment if its there
+  }
+  let max = 0
+  let maxCharacter = ''
   
+  for (let j in charMap){
+    if(charMap[j] > max){
+        max = charMap[j]
+        maxCharacter = j
+    }
+  }
+
+  return maxCharacter
 };
 
 module.exports = maxCharacter;
