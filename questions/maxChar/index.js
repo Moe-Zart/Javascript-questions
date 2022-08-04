6e85bcb87bf4945e7cb8d20d8914d8504e9e320c
Moe-Zart <mohamedzreika03@outlook.com>
2022-08-05 08:23:09 +1000

    Character Map

--- questions/maxChar/index.js
+++ questions/maxChar/index.js
@@ -5,9 +5,10 @@
  * maxCharacter('aabbbccd') === 'b'
  * maxCharacter('foo 111123') === '1'
  */
-
+/*
 const maxCharacter = (str) => {
     //S1mpl1f1ed
+    
   let charMap = {}; // empty object
   for (let i of str) { //loops through every iterable object of str (each letter)
     !charMap[i] ? charMap[i] = 1 : charMap[i] += 1 //if there is no charMap[letter], then return the letter with value of one, otherwise, increment if its there
@@ -23,6 +24,21 @@ const maxCharacter = (str) => {
   }
 
   return maxCharacter
+};
+*/
+let max = 0;
+let maxChar = "";
+const maxCharacter = (str) => {
+  //split
+  //loop through array and create character map for each letter
+
+  let strArr = str.split("");
+  let charMap = new Map();
+
+  for (let char of strArr) {
+    charMap.set(char, charMap.get(char) + 1 || 1);
+  }
+  console.log(charMap);
 };
 
 module.exports = maxCharacter;
